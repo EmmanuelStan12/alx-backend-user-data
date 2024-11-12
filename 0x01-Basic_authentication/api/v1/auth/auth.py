847f74142866ff30e27be90473b139e6803bc4ca
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-11-12 19:51:16 +0100

    task: Define which routes need auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Auth class
 """
+import re
 from flask import request
 from typing import List, TypeVar
 
@@ -12,11 +13,24 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks the path that requires auth
         """
-        return False
+        if path is not None and excluded_paths is not None:
+            for excluded_path in excluded_paths:
+                pattern = ''
+                if excluded_path[-1] == '*':
+                    pattern = '{}.*'.format(excluded_path[0:-1])
+                elif excluded_path[-1] == '/':
+                    pattern = '{}/*'.format(excluded_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(excluded_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Checks the authorization header
         """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
