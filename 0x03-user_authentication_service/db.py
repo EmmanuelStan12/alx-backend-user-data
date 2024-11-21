1b3331b43501ff20ec8c89a2b21e1c09db52585c
Emmanuel Stanley <emmanuelstanley753@gmail.com>
2024-11-21 21:20:55 +0100

    fix: Exception thrown

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,13 +32,17 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Adds a user to the database
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
+            return new_user
+        except Exception:
+            self._session.rollback()
+        return None
 
     def find_user_by(self, **kwargs):
         """Find the first user that matches the args.
